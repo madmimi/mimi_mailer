8d9edf5ba737d49b5a3532776468506c4fdd4e80
Jeff Browning <jeff@jkbrowning.com>
2014-01-10 13:51:48 -0500

    Test .mail and .mail_plain_test

--- lib/mimi_mailer/base.rb
+++ lib/mimi_mailer/base.rb
@@ -1,44 +1,42 @@
 module MimiMailer
-  class Error < StandardError; end
-
   class Base
     include HTTParty
     base_uri 'https://api.madmimi.com'
 
-    def self.deliver(object_id, args)
-      raise NotImplementedError.new("#{self.class.name}#deliver must be overriden")
+    def self.deliver(*args)
+      raise NotImplementedError.new("#{self.class.name}.deliver must be overridden")
     end
 
     def self.mail(promotion_name, subject, to_address, body = {})
-      return rand(20_000_000_000) unless MimiMailer.deliveries_enabled?
-
-      response = post('/mailer', body: {
-        username:       MimiMailer.config.username,
-        api_key:        MimiMailer.config.api_key,
-        recipient:      to_address,
-        subject:        subject,
-        from:           MimiMailer.config.default_from_address,
-        promotion_name: promotion_name,
-        body:           body.to_yaml
-      })
-      transaction_id = response.body.to_i
-      transaction_id
+      if MimiMailer.deliveries_enabled?
+        response = post('/mailer', body: {
+          username:       MimiMailer.config.username,
+          api_key:        MimiMailer.config.api_key,
+          recipient:      to_address,
+          subject:        subject,
+          from:           MimiMailer.config.default_from_address,
+          promotion_name: promotion_name,
+          body:           body.to_yaml
+        })
+        transaction_id = response.body.to_i
+        transaction_id
+      end
     end
 
     def self.mail_plain_text(promotion_name, subject, to_address, body)
-      return rand(20_000_000_000) unless MimiMailer.deliveries_enabled?
-
-      response = post('/mailer', body: {
-        username:       MimiMailer.config.username,
-        api_key:        MimiMailer.config.api_key,
-        recipient:      to_address,
-        subject:        subject,
-        from:           MimiMailer.config.default_from_address,
-        promotion_name: promotion_name,
-        raw_plain_text: body
-      })
-      transaction_id = response.body.to_i
-      transaction_id
+      if MimiMailer.deliveries_enabled?
+        response = post('/mailer', body: {
+          username:       MimiMailer.config.username,
+          api_key:        MimiMailer.config.api_key,
+          recipient:      to_address,
+          subject:        subject,
+          from:           MimiMailer.config.default_from_address,
+          promotion_name: promotion_name,
+          raw_plain_text: body
+        })
+        transaction_id = response.body.to_i
+        transaction_id
+      end
     end
   end
 end
--- spec/mimi_mailer/base_spec.rb
+++ spec/mimi_mailer/base_spec.rb
@@ -1,9 +1,224 @@
 require 'spec_helper'
 
 describe MimiMailer::Base do
-  class TestMailer < MimiMailer::Base; end
+  describe ".deliver" do
+    class TestMailer < MimiMailer::Base; end
 
-  it "should have specs" do
-    pending "Add specs to me!"
+    subject { TestMailer }
+
+    it "raies an error if not implemented" do
+      expect {
+        subject.deliver
+      }.to raise_error(NotImplementedError, /overridden/i)
+    end
+  end
+
+  describe ".mail" do
+    let(:promotion_name) { "kittens" }
+    let(:email_subject) { "omg kittens" }
+    let(:to_address) { "dog@thepound.org" }
+    let(:body) { { cats: "rule", dogs: "drool" } }
+    let(:username) { "cats@themansion.org" }
+    let(:api_key)  { "apikey" }
+    let(:default_from) { "catsinthecradle@themansion.com" }
+
+    subject { MimiMailer::Base }
+
+    before do
+      stub_request(:post, "https://api.madmimi.com/mailer")
+      MimiMailer.config.stub(
+        username: username, api_key: api_key, default_from_address: default_from)
+      MimiMailer.stub(deliveries_enabled?: true)
+      subject.stub(from_address: default_from)
+    end
+
+    it "posts to the correct API endpoint" do
+      subject.mail(promotion_name, email_subject, to_address, body)
+      expect(a_request(:post, "https://api.madmimi.com/mailer")).to have_been_requested
+    end
+
+    it "passes in the correct the username" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(username: username))
+
+      subject.mail(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the correct the api_key" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(api_key: api_key))
+
+      subject.mail(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the right subject" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(subject: email_subject))
+
+      subject.mail(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the right promotion name" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(promotion_name: promotion_name))
+
+      subject.mail(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the right recipient" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(recipient: to_address))
+
+      subject.mail(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the yaml-converted body" do
+      expected_body = body.to_yaml
+      
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(body: expected_body))
+
+      subject.mail(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "returns the Mimi transaction ID" do
+      transaction_id = rand(1000)
+      stub_request(:post, "https://api.madmimi.com/mailer").to_return(
+        body: transaction_id.to_s)
+
+      result = subject.mail(promotion_name, email_subject, to_address, body)
+      expect(result).to eql(transaction_id)
+    end
+
+    it "passes in the right from address" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(from: default_from))
+
+      result = subject.mail(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    context "when deliveries are disabled" do
+
+      before { MimiMailer.stub(deliveries_enabled?: false) }
+
+      it "does not post anything" do
+        request = stub_request(:post, "https://api.madmimi.com/mailer")
+        result = subject.mail(promotion_name, email_subject, to_address, body)
+        expect(request).to_not have_been_requested
+      end
+    end
+  end
+
+  describe ".mail_plain_text" do
+    let(:promotion_name) { "kittens" }
+    let(:email_subject) { "omg kittens" }
+    let(:to_address) { "dog@thepound.org" }
+    let(:body) { "cats rule and dogs drool" }
+    let(:username) { "cats@themansion.org" }
+    let(:api_key)  { "apikey" }
+    let(:default_from) { "catsinthecradle@themansion.com" }
+
+    subject { MimiMailer::Base }
+
+    before do
+      stub_request(:post, "https://api.madmimi.com/mailer")
+      MimiMailer.config.stub(
+        username: username, api_key: api_key, default_from_address: default_from)
+      MimiMailer.stub(deliveries_enabled?: true)
+      subject.stub(from_address: default_from)
+    end
+
+    it "posts to the correct API endpoint" do
+      subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(a_request(:post, "https://api.madmimi.com/mailer")).to have_been_requested
+    end
+
+    it "passes in the correct the username" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(username: username))
+
+      subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the correct the api_key" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(api_key: api_key))
+
+      subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the right subject" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(subject: email_subject))
+
+      subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the right promotion name" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(promotion_name: promotion_name))
+
+      subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the right recipient" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(recipient: to_address))
+
+      subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "passes in the unmodified body" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(raw_plain_text: body))
+
+      subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    it "returns the Mimi transaction ID" do
+      transaction_id = rand(1000)
+      stub_request(:post, "https://api.madmimi.com/mailer").to_return(
+        body: transaction_id.to_s)
+
+      result = subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(result).to eql(transaction_id)
+    end
+
+    it "passes in the right from address" do
+      request = stub_request(:post, "https://api.madmimi.com/mailer").with(
+        body: hash_including(from: default_from))
+
+      result = subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+      expect(request).to have_been_requested
+    end
+
+    context "when deliveries are disabled" do
+      before { MimiMailer.stub(deliveries_enabled?: false) }
+
+      it "does not post anything" do
+        request = stub_request(:post, "https://api.madmimi.com/mailer")
+        result = subject.mail_plain_text(promotion_name, email_subject, to_address, body)
+        expect(request).to_not have_been_requested
+      end
+    end
+  end
+
+  describe ".from_address" do
+    it "sets the from address"
+    it "returns the from address"
+    it "returns the default from address is the class doesn't have its own"
   end
 end
